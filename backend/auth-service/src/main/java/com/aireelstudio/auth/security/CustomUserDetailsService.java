5253e8a975f81860f1c3a8d50bf87329567fd937
Gadi Venkatesh <85879089+GadiVenkatesh@users.noreply.github.com>
2026-06-20 22:54:00 +0530

    Add CustomUserDetailsService

---
+++ backend/auth-service/src/main/java/com/aireelstudio/auth/security/CustomUserDetailsService.java
@@ -0,0 +1,18 @@
+package com.aireelstudio.auth.security;
+
+import com.aireelstudio.auth.entity.User;
+import com.aireelstudio.auth.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+ private final UserRepository userRepository;
+ @Override
+ public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+  User user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+  return org.springframework.security.core.userdetails.User.withUsername(user.getEmail()).password(user.getPassword()).authorities("ROLE_"+user.getRole().name()).build();
+ }
+}
